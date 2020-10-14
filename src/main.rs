1d4f358354c780085ac934221dca14fe1eca1a25
loc <mao.l.chong@gmail.com>
2020-10-14 23:38:19 +0800

    update 数据类型和函数

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ mod common_programming_concepts;
 // use guess::game::guess_game;
 use common_programming_concepts::variables::*;
 use common_programming_concepts::data_types::*;
+use common_programming_concepts::functions_work;
+use crate::common_programming_concepts::functions_work::func_parameters;
+
 fn main() {
     // guess_game()
-    var();
-    println!("heartbeat is {}", HEARTBEAT_127);
-    shadowing();
+    // var();
+    // println!("heartbeat is {}", HEARTBEAT_127);
+    // shadowing();
 
-    statically_typed();
-    scalar();
+    // statically_typed();
+    // scalar();
+    // compound_types();
+    functions_work::this_is_function();
+    functions_work::func_parameters(5, 6);
+    functions_work::statements_expressions();
+    println!("return value is {}", functions_work::return_value());
 }
